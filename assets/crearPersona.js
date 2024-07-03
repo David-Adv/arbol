29e0d88f6b261b481ed9aa1ad02820ab92e37d4c
David Vazquez <capacitaciones06.13@adviters.com>
2024-07-03 15:53:57 -0300

    mejora de codigo

--- assets/crearPersona.js
+++ assets/crearPersona.js
@@ -2,16 +2,12 @@
 
 export function crearPersona(nombre, predecesor, tipoPredecesor) {
    
-    const persona = {
-        nombre:"",
-        predecesor:"",
-        tipoPredecesor:""
+    return {
+        nombre,
+        predecesor,
+        tipoPredecesor
     }
 
-    persona.nombre = nombre;
-    persona.predecesor = predecesor;
-    persona.tipoPredecesor = tipoPredecesor
 
-    return persona
 }
  
